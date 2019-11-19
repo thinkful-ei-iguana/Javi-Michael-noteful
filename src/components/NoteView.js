79571cc5ff39202a1775cdd52ea6894b616f6479
Michael Weedman <michael.weedman@gmail.com>
2019-11-19 13:46:06 -0700

    added list id to note view.

--- src/components/NoteView.js
+++ src/components/NoteView.js
@@ -5,14 +5,19 @@ export default function NoteView(props) {
   console.log("noteView:", props);
 
   return (
-    <div>
-      <Note
-        name={props.notes.name}
-        key={props.notes.id}
-        modified={props.notes.modified}
-        id={props.notes.id}
-      />
-      <p>{props.notes.content}</p>
+    <div id="wrapper">
+      <section>
+        <h4>{props.notes.folderId}</h4>
+      </section>
+      <main>
+        <Note
+          name={props.notes.name}
+          key={props.notes.id}
+          modified={props.notes.modified}
+          id={props.notes.id}
+        />
+        <p>{props.notes.content}</p>
+      </main>
     </div>
   );
 }
