6bd70e871fc94f4a5d748bcbd141bc66a4e4c051
Michael Weedman <michael.weedman@gmail.com>
2019-11-20 07:18:30 -0700

    Clean up

--- src/components/FolderView.js
+++ src/components/FolderView.js
@@ -1,17 +1,21 @@
 import React from "react";
 import Folders from "./Folders";
+import NotFound from "./NotFound";
 import Notes from "./Notes";
 
 export default function FolderView(props) {
-  console.log(props);
-  return (
-    <div id="wrapper">
-      <section>
-        <Folders folders={props.folders} />
-      </section>
-      <main>
-        <Notes notes={props.notes} />
-      </main>
-    </div>
-  );
+  if (!props.folders) {
+    return <NotFound />;
+  } else {
+    return (
+      <div id="wrapper">
+        <section>
+          <Folders folders={props.folders} />
+        </section>
+        <main>
+          <Notes notes={props.notes} />
+        </main>
+      </div>
+    );
+  }
 }
